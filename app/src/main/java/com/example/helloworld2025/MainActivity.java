e92ef4853a36d92a06ed4d721922fafbec0af208
Rajan Balami <rajanbalami109@gmail.com>
2025-09-24 23:17:09 +0300

    Added extra button and event handling logic

--- app/src/main/java/com/example/helloworld2025/MainActivity.java
+++ app/src/main/java/com/example/helloworld2025/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.helloworld2025;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDemoLabel;
+    Button changeColorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        this.tvDemoLabel = findViewById(R.id.tvDemoLabel);
+        changeColorButton = findViewById(R.id.btnChangeColor);
+        changeColorButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                tvDemoLabel.setBackgroundColor(Color.YELLOW);//Example: Change color to Yellow
+            }
+        });
+    }
+
+    public void changeTextClick(View view) {
+        tvDemoLabel.setText("Hello World! Changed");
     }
 }
